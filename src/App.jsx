eb91a1c74245608c2a5b9113d587eb2fb376a21d
Julien Levieux <julien.levieux@drotek.com>
2023-10-26 19:22:19 +0200

    add dependencies + use browserRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,20 @@
 import React from "react";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
-// import pages
 import CreateEmployee from "./Pages/CreateEmployee/index.jsx";
 import EmployeesList from "./Pages/EmployeeList/index.jsx";
 
 import "./App.css";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <CreateEmployee />,
-    children: [],
-  },
-
-  {
-    path: "list",
-    element: <EmployeesList />,
-  },
-]);
-
 function App() {
-  return <RouterProvider router={router} />;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<CreateEmployee />} />
+        <Route path="/list" element={<EmployeesList />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
