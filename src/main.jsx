17f745b02a8a26afcc33b9d79b7330842683c145
Julien Levieux <julien.levieux@drotek.com>
2023-11-08 12:11:36 +0100

    deploy: make it work with React 18

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import { Provider } from 'react-redux';
-import store from './redux/Store.js';
-import { BrowserRouter } from 'react-router-dom';
+// main.jsx
+import React from "react";
+import { createRoot } from "react-dom";
+import App from "./App.jsx";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./redux/Store.js";
 
-ReactDOM.render(
+createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
         <App />
-      </BrowserRouter>
     </Provider>
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
